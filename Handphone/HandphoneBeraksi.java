06d0e8deb75894f7f67b9ea13e9dd753bd083735
kismanprojectUas <kismanp35@gmail.com>
2025-01-03 18:26:38 +0700

    Create HandphoneBeraksi.java

---
+++ Handphone/HandphoneBeraksi.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Handphone;
+
+/**
+ *
+ * @author sanda
+ */
+public class Hanphoneberaksi {
+    public static void main(String[] args) {
+        
+        Handphone HP = new Handphone();
+        
+        HP.Hidupkan();
+        HP.MelakukanPanggilan();
+        HP.KirimSms();
+        HP.Matikan();
+    }
+    
+}
