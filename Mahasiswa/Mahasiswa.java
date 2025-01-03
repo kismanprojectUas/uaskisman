2557df158dd5c3aaf384c686eab3ebaf0a7fcc91
kismanprojectUas <kismanp35@gmail.com>
2025-01-03 18:27:52 +0700

    Create Mahasiswa.java

---
+++ Mahasiswa/Mahasiswa.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Mahasiswa;
+
+/**
+ *
+ * @author KISMAN
+ */
+public class Mahasiswa {
+    
+        void Membaca(){
+            System.out.println("Mahasiswa diharapkan untuk membaca aturan terlebi dahulu sebelum Ulangan dimulai");
+        }
+        void Nyontek(){
+            System.out.println("Dilarang untuk menyontek ketika Ulangan, Tetapi boleh membuka buku");
+        }
+        void Modifikasi(){
+            System.out.println("Mahasiswa boleh memodifikasi dari buku yang anda baca");
+        }
+}
