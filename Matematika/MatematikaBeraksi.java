3be8a431346586c728717ee4426aa56f8db52b7c
kismanprojectUas <kismanp35@gmail.com>
2025-01-03 18:42:27 +0700

    Create MatematikaBeraksi.java

---
+++ Matematika/MatematikaBeraksi.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Matematika;
+
+/**
+ *
+ * @author KISMAN
+ */
+public class MatematikaBeraksi {
+    
+    public static void main(String[] args) {
+        
+        Matematika mtk = new Matematika();
+        
+        System.out.println("Pertambahan 20 + 20 = " + mtk.pertambahan(20, 20));
+        System.out.println("Pengurangan 20 + 20 = " + mtk.pengurangan(10, 5));
+        System.out.println("Perkalian 20 + 20 = " + mtk.perkalian(10, 20));
+        System.out.println("Pembagian 20 + 20 = " + mtk.pembagian(21, 2));
+    }
+    
+}
