375652ab042a77eb03589003b1c222950e258189
kismanprojectUas <kismanp35@gmail.com>
2025-01-03 18:31:37 +0700

    Create Matematika.java

---
+++ MatematikCanggih/Matematika.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MatematikCanggih;
+
+/**
+ *
+ * @author KISMAN
+ */
+public class Matematika {
+    public int pertambahan(int a, int b) {
+        return a + b;
+    }
+
+    public int pengurangan(int a, int b) {
+        return a - b;
+    }
+
+    public int perkalian(int a, int b) {
+        return a * b;
+    }
+
+    public int pembagian(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Pembagian oleh nol tidak diperbolehkan.");
+        }
+        return a / b;
+    }
+}
+
