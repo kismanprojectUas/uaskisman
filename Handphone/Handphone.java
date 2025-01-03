654c3a671639f408ddd4e20a552dec432793d6e5
kismanprojectUas <kismanp35@gmail.com>
2025-01-03 18:25:16 +0700

    Create Handphone.java

---
+++ Handphone/Handphone.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Handphone;
+
+/**
+ *
+ * @author sanda
+ */
+public class Handphone {
+    
+        void Hidupkan(){
+            System.out.println("Hanphone IPHONE hidup");
+        }
+        void MelakukanPanggilan(){
+            System.out.println("Melakuan Panggilan........kringggg.......kringggg.....");
+        }
+        void KirimSms(){
+            System.out.println("Panggilan tidak diterima......kirim sms");
+        }
+        void Matikan(){
+            System.out.println("Handphone Mati");
+        }
+}
