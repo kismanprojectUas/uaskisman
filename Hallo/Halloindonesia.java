631bb845eb7ab32673bd804bb1519d503386954f
kismanprojectUas <kismanp35@gmail.com>
2025-01-03 18:23:13 +0700

    Create Halloindonesia.java

---
+++ Hallo/Halloindonesia.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Hallo;
+
+/**
+ *
+ * @author KISMAN
+ */
+public class Halloindonesia {
+
+    public static void main(String[] args) {
+        System.out.println("Hallo indonesia");
+        System.out.println("Selamat pagi indonesia");
+        System.out.println("Jaya indonesia");
+    }
+}
