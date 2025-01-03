8855d5d718630f2c610a45a0b55be6b6c35c2f91
kismanprojectUas <kismanp35@gmail.com>
2025-01-03 18:11:37 +0700

    Create AC.java

---
+++ AC/AC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AC;
+
+/**
+ *
+ * @author KISMAN
+ */
+public interface AC {
+    void matikanAC();
+    void hidupkanAC();
+    void dinginkanAC();
+    void panaskanAC();
+}
