baeca4689f661b3422f723e066b4dabb9d73ffbf
kismanprojectUas <kismanp35@gmail.com>
2025-01-03 18:39:18 +0700

    Create MatematikaCanggihBeraksi.java

---
+++ MatematikCanggih/MatematikaCanggihBeraksi.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MatematikCanggih;
+
+/**
+ *
+ * @author KISMAN
+ */
+public class MatematikaCanggihBeraksi {
+    public static void main(String[] args) {
+        MatematikaCanggih mc = new MatematikaCanggih();
+
+        System.out.println("Pertambahan: 10 + 20 = " + mc.pertambahan(10, 20));
+        System.out.println("Perkalian: 10 * 5 = " + mc.perkalian(10, 5));
+        System.out.println("Modulus: 10 % 3 = " + mc.modulus(10, 3));
+    }
+}
